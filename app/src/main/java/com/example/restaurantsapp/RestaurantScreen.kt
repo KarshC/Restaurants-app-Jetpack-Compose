e92d77731fc8d6823bf134e15fa729c038d5aaf2
karshC <karshpsu@gmail.com>
2023-08-03 13:33:05 -0400

    Adding favorite icon

--- app/src/main/java/com/example/restaurantsapp/RestaurantScreen.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantScreen.kt
@@ -1,19 +1,26 @@
 package com.example.restaurantsapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -50,10 +57,33 @@ fun RestaurantItem(item : Restaurant){
             RestaurantDetails(
                 item.title,
                 item.description,
-                Modifier.weight(0.85f)
+                Modifier.weight(0.7f)
+            )
+            FavoriteIcon(
+                Modifier.weight(0.15f)
             )
         }
     }
+}
+
+@Composable
+fun FavoriteIcon(modifier: Modifier) {
+    val favoriteState = remember {
+        mutableStateOf(false)
+    }
+    val icon = if (favoriteState.value){
+        Icons.Filled.Favorite
+    } else {
+        Icons.Filled.FavoriteBorder
+    }
+    Image(
+        imageVector = icon,
+        contentDescription = "Favorite restaurant icon",
+        modifier = modifier.padding(8.dp)
+            .clickable {
+                favoriteState.value = !favoriteState.value
+            }
+    )
 }
 
 @Composable
