716b90d93cc504ee023ea4eb5981d41afc5c78a5
karshC <karshpsu@gmail.com>
2023-08-08 08:42:29 -0400

    state hoisting to viewmodel (only source of truth)

--- app/src/main/java/com/example/restaurantsapp/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.restaurantsapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class RestaurantViewModel : ViewModel(){
-    fun getRestaurants() = dummyRestaurants
+class RestaurantViewModel : ViewModel() {
+    val state = mutableStateOf(dummyRestaurants)
+
+    fun toggleFavorite(id: Int) {
+        val restaurants = state.value.toMutableList()
+        val itemIndex = restaurants.indexOfFirst {
+            it.id == id
+        }
+        val item = restaurants[itemIndex]
+        restaurants[itemIndex] = item.copy(isFavorite = !item.isFavorite)
+        state.value = restaurants
+    }
 }
