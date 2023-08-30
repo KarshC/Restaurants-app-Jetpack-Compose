3ae46bfc4ca81c8154d4efe95fe62c6adb5cefa9
karshC <karshpsu@gmail.com>
2023-08-30 18:19:20 -0400

    Adding GET call for restaurant details screen

--- app/src/main/java/com/example/restaurantsapp/RestaurantApiService.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantApiService.kt
@@ -1,8 +1,14 @@
 package com.example.restaurantsapp
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RestaurantApiService {
     @GET("restaurants.json")
     suspend fun getRestaurants(): List<Restaurant>
+
+    @GET("restaurants.json?orderBy=\"r_id\"")
+    suspend fun getRestaurantDetails(
+        @Query("equalTo") id: Int
+    ): Map<String, Restaurant>
 }
