90d6fe7be0c9d3a6053dd8207c57a9bbd83b6872
karshC <karshpsu@gmail.com>
2023-10-10 17:40:19 -0400

    adding loading indicator

--- app/src/main/java/com/example/restaurantsapp/RestaurantScreen.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantScreen.kt
@@ -2,13 +2,16 @@ package com.example.restaurantsapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
@@ -30,21 +33,28 @@ import com.example.restaurantsapp.ui.theme.RestaurantsAppTheme
 @Composable
 fun RestaurantScreen(onItemClick: (id: Int) -> Unit = {}) {
     val viewModel: RestaurantViewModel = viewModel()
-
-    LazyColumn(
-        contentPadding = PaddingValues(
-            vertical = 8.dp,
-            horizontal = 8.dp
-        )
+    val restaurants = viewModel.state.value
+    val isLoading = restaurants.isEmpty()
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
-        items(viewModel.state.value) { restaurant ->
-            RestaurantItem(restaurant,
-                onFavoriteClick = { id, oldValue ->
-                    viewModel.toggleFavorite(id, oldValue)
-                },
-                onItemClick = { id -> onItemClick(id) }
+        LazyColumn(
+            contentPadding = PaddingValues(
+                vertical = 8.dp,
+                horizontal = 8.dp
             )
+        ) {
+            items(restaurants) { restaurant ->
+                RestaurantItem(restaurant,
+                    onFavoriteClick = { id, oldValue ->
+                        viewModel.toggleFavorite(id, oldValue)
+                    },
+                    onItemClick = { id -> onItemClick(id) }
+                )
+            }
         }
+        if (isLoading) CircularProgressIndicator()
     }
 }
 
